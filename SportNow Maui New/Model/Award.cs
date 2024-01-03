caec7f2bbb139315a7cd970803ab296904815e19
hmapedro <78369129+hmapedro@users.noreply.github.com>
2024-01-03 15:54:22 +0000

    Add files via upload

---
+++ SportNow Maui New/Model/Award.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SportNow.Model
+{
+    public class Objective
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string epoca { get; set; }
+        public string objectivos { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+}
---
+++ SportNow Maui New/Platforms/iOS/AppDelegateEx.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using Microsoft.Extensions.Logging;
+using UIKit;
+
+namespace SportNow
+{
+    
+    public abstract class AppDelegateEx : MauiUIApplicationDelegate
+    {
+        public virtual UIInterfaceOrientationMask CurrentLockedOrientation { get; set; }
+
+        //according to the Apple docs, Application and ViewController have to agree on the supported orientation, this forces it
+        //https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623107-application?language=objc
+        [Foundation.Export("application:supportedInterfaceOrientationsForWindow:")]
+        public virtual UIInterfaceOrientationMask GetSupportedInterfaceOrientationsForWindow(UIApplication application, UIWindow forWindow)
+            => this.CurrentLockedOrientation;
+    }
+}
