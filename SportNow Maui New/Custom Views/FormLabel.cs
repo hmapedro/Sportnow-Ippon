a4ee7e266650c3d56ecdf1fb2e81fcc3624d1855
hmapedro <78369129+hmapedro@users.noreply.github.com>
2024-01-03 15:52:57 +0000

    Add files via upload

---
+++ SportNow Maui New/Custom Views/FormLabel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Maui;
+
+namespace SportNow.CustomViews
+{
+    public class FormLabel: Label
+    {
+
+
+        public FormLabel()
+        {
+            VerticalTextAlignment = TextAlignment.Center;
+            HorizontalTextAlignment = TextAlignment.Start;
+            TextColor = Colors.White;
+            LineBreakMode = LineBreakMode.NoWrap;
+            Padding = 0;
+            FontFamily = "futuracondensedmedium";
+            FontSize = App.formLabelFontSize;
+        }
+    }
+}
---
+++ SportNow Maui New/Custom Views/ServiceBox.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SportNow.CustomViews
+{
+    public class ServiceBox : AbsoluteLayout
+    {
+        public Label label;
+		public Frame frame;
+
+
+		public ServiceBox(string text, double width, double height)
+        {
+			this.WidthRequest = width;
+			this.HeightRequest = height;
+			frame = new Frame
+			{
+				CornerRadius = 5,
+				IsClippedToBounds = true,
+				BorderColor = App.topColor,
+				BackgroundColor = Colors.Transparent,
+				Padding = new Thickness(2, 2, 2, 2),
+				HeightRequest = height,
+                WidthRequest = width,
+                VerticalOptions = LayoutOptions.Center,
+			};
+
+			this.Children.Add(frame);
+            this.SetLayoutBounds(frame, new Rect(0, 0, width - 5 * App.screenHeightAdapter, height));
+
+			label = new Label { LineBreakMode = LineBreakMode.WordWrap, BackgroundColor = Colors.Transparent, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center, FontSize = App.titleFontSize, TextColor = App.bottomColor };
+			label.Text = text;
+
+			this.Children.Add(label);
+            this.SetLayoutBounds(label, new Rect(5 * App.screenWidthAdapter, 5 * App.screenHeightAdapter, width - 10 * App.screenWidthAdapter, height - 10 * App.screenHeightAdapter));
+
+			//return itemabsoluteLayout;
+		}
+    }
+}
